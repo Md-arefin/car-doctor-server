31c8e2f7fb62b9c506d5708eb8a358f1687798e4
Md-arefin <ansariarefin617@gmail.com>
2023-05-12 15:27:32 +0600

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const express = require("express");
 const cors = require('cors');
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware 
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 
@@ -29,10 +29,46 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const serviceCollection = client.db("carDoctor").collection("services");
+        const bookingCollection = client.db("carDoctor").collection("bookings");
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, price: 1, service_id: 1, img:1 },
+            };
+
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+
+
+
+        // bookings
+
+        app.post('/bookings', async (req, res) =>{
+            const booking = req.body; 
+            console.log(booking)
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        
     } finally {
+
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
@@ -48,7 +84,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Car doctor server is running on Port: ${port}`)
 })
-
-// PbUVquXK7tgwbWT9
-
-// ansariarefin617
